322078cba77f99cf30b913614cf7833a0f905b45
soyeongame <81913158+soyeongame@users.noreply.github.com>
2021-04-13 01:56:25 +0900

    1

--- js/main.js
+++ js/main.js
@@ -819,6 +819,7 @@ window.onresize = function()
 }
 
 function addEvent()
+
 {
 	document.addEventListener('mousemove',function(e)
 	{
@@ -965,3 +966,18 @@ function pause() {
 
 init(true);
 addEvent();
+
+document.documentElement.addEventListener('touchstart', function (event) {
+     if (event.touches.length > 1) {
+          event.preventDefault(); 
+        } 
+    }, false);
+
+var lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+     var now = (new Date()).getTime();
+     if (now - lastTouchEnd <= 300) {
+          event.preventDefault(); 
+        } lastTouchEnd = now; 
+    }, false);
