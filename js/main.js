4c730c8c655c426f807689add364aaef99b21e17
soyeongame <81913158+soyeongame@users.noreply.github.com>
2021-04-13 02:53:57 +0900

    a

--- js/main.js
+++ js/main.js
@@ -967,3 +967,11 @@ function pause() {
 init(true);
 addEvent();
 
+var lastTouchEnd= 0;
+document.addEventListener('touchend', function (event) {
+    var now= (new Date()).getTime();
+    if (now -lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd= now;
+}, false);
